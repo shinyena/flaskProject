835e8643866bd48de48557461dee2bfd7629e6c9
신예나 <48668024+shinyena@users.noreply.github.com>
2022-10-22 14:42:45 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/ml', methods = ['post', 'get'])
+def ml():
+    ml_url = request.form.get("ml_url")
+    # 머신러닝 코드
+    return render_template('ml.html', ml_result=ml_url)
+
+
+if __name__ == '__main__':
+    app.run()
